a755ecdcad6a9b76467923493aad06e139fa05a5
Dinesh Mathur <dinesh.mathur@volue.com>
2022-01-17 09:45:12 +0100

    Add total

--- Frontend/smart-hardware-app/src/app/app.component.ts
+++ Frontend/smart-hardware-app/src/app/app.component.ts
@@ -13,20 +13,27 @@ export class AppComponent {
 
   public products$: Observable<Product[]> | undefined;
   public checkoutList: Product[] = new Array<Product>();
+  public totalPrice: number = 0;
 
   constructor(private productService: ProductService) {
   }
 
   addProduct(product: Product) {
     this.checkoutList.push(product);
+    this.totalPrice = this.checkoutList.map(item => item.price)
+                        .reduce((sum, current) => sum + current, 0);
   }
   removeProduct(index: number) {
     this.checkoutList.splice(index, 1);
+    this.totalPrice = this.checkoutList.map(item => item.price)
+                        .reduce((sum, current) => sum + current, 0);
   }
   removeAll() {
     this.checkoutList = [];
+    this.totalPrice = this.checkoutList.map(item => item.price)
+                        .reduce((sum, current) => sum + current, 0);
   }
-  searchTextChanged(event: any){
+  searchTextChanged(event: any) {
     this.products$ = this.productService.getProducts(event.target.value);
   }
 
